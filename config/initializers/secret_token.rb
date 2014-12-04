4c6afaab97b67dae2d4d5ee5a540a5b3566303b6
jesuisuncaillou <igorlacroute@gmail.com>
2014-12-04 12:32:45 +0100

    init des gems

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EvryGameCity::Application.config.secret_key_base = 'de5aa0308bff6e906a681698efb61168393aa0fb156c1cfc7031538a0b270beb10df777db8b058da8b5e6bdb6a56d27749a4fb71df172738d1aec22657f54b37'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EvryGameCity::Application.config.session_store :cookie_store, key: '_evry_game_city_session'
