329d271408b47daa08cb7984be1ca5e4cb74a118
jesuisuncaillou <igorlacroute@gmail.com>
2014-12-04 18:31:50 +0100

    dropdown bootstrap fix bis

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
-//= require turbolinks
 //= require_tree .
 
-$(document).ready(function() { $('.dropdown-toggle').dropdown(); });
+$(document).ready(function() {
+  $('.dropdown-toggle').dropdown();
+});
