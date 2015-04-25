8371a1c32d5999009efc1d92d693d386fb34716a
jesuisuncaillou <igorlacroute@gmail.com>
2015-04-25 15:09:30 +0200

    +stream

--- db/migrate/20150425124038_new_stream.rb
+++ db/migrate/20150425124038_new_stream.rb
@@ -1,5 +1,7 @@
 class NewStream < ActiveRecord::Migration
   def change
-    Stream.create(name: "EGC", href: "//games.dailymotion.com/embed/x2jy5eh", jeu: "all")
+    Stream.create(name: "War Legend", href: "http://www.twitch.tv/warlegend/embed", jeu: "lol")
+    Stream.create(name: "EGC Dailymotion", href: "//games.dailymotion.com/embed/x2jy5eh", jeu: "all")
+    Stream.create(name: "EGC Twitch", href: "http://www.twitch.tv/mastersjeuvideo/embed", jeu: "all")
   end
 end
